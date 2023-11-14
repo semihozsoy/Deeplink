2aea8bc467ba197c023bd7bbcb0afec3ce54d6dd
Semih Özsoy <semih__ozsoy@hotmail.com>
2023-11-14 14:16:44 +0300

    dependency inversion principle implementation

---
+++ Sources/Deeplink/ApplinkRoutable.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Semih Ozsoy on 13.11.2023.
+//
+
+import Foundation
+
+/// Routing conformance for the Main scene.
+
+public protocol ApplinkRoutable {
+    /// Should be implemented for each link.
+}
